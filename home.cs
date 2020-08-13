52cace28858ce59b263d2bbaa76892d41e8bfedc
Hla Mansour <40806663+hlaahmed@users.noreply.github.com>
2020-08-13 04:16:25 +0200

    .

---
+++ home.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScannerProject
+{
+    public partial class home : Form
+    {
+        public home()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            write page = new write();
+            page.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            import page = new import();
+            page.ShowDialog();
+        }
+
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new home());
+        }
+    }
+}
